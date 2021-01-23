c70b90670cfb96a94b46402c8bab95d3c8f87c54
Tyler <tystuyfzand@gmail.com>
2021-01-23 18:55:37 -0500

    Add ChannelType middleware

--- middleware/channels.go
+++ middleware/channels.go
@@ -2,13 +2,14 @@ package middleware
 
 import (
 	"errors"
+	"github.com/diamondburned/arikawa/v2/discord"
 	"meow.tf/astral/router"
 )
 
 // Errors
 var (
-	ErrOnCooldown     = errors.New("command is on cooldown")
 	ErrChannelNotNSFW = errors.New("this command can only be used in an NSFW channel")
+	ErrChannelType    = errors.New("channel type does not match expected type")
 )
 
 // RequireNSFW requires a message to be sent from an NSFW channel
@@ -17,6 +18,19 @@ func RequireNSFW(catch CatchFunc) router.MiddlewareFunc {
 		return func(ctx *router.Context) {
 			if !ctx.Channel.NSFW {
 				callCatch(ctx, catch, ErrChannelNotNSFW)
+				return
+			}
+			fn(ctx)
+		}
+	}
+}
+
+// ChannelType requires the specific channel type from the message
+func ChannelType(t discord.ChannelType, catch CatchFunc) router.MiddlewareFunc {
+	return func(fn router.Handler) router.Handler {
+		return func(ctx *router.Context) {
+			if ctx.Channel.Type != t {
+				callCatch(ctx, catch, ErrChannelType)
 				return
 			}
 			fn(ctx)
