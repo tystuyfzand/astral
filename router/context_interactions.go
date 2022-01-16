97a4b2b1a4c2477870bdc7e4681a4319c894b143
Tyler <tystuyfzand@gmail.com>
2022-01-16 05:13:56 -0500

    Use case insensitive matching on arguments

--- router/context_interactions.go
+++ router/context_interactions.go
@@ -5,6 +5,7 @@ import (
 	"github.com/diamondburned/arikawa/v3/gateway"
 	"github.com/diamondburned/arikawa/v3/state"
 	"strconv"
+	"strings"
 )
 
 // ContextFromInteraction creates a new Context from an interaction event
@@ -30,44 +31,54 @@ func ContextFromInteraction(state *state.State, event *gateway.InteractionCreate
 		data := event.Data.(*discord.CommandInteraction)
 
 		for _, opt := range data.Options {
-			arg, ok := r.Arguments[opt.Name]
-
-			if !ok {
+			// TODO: Is this even valid?
+			// This skips values which are simply subcommand types...
+			if opt.Value == nil {
 				continue
 			}
 
-			var val string
+			for _, arg := range r.Arguments {
+				argName := strings.ToLower(commandNameRe.ReplaceAllString(strings.ToLower(arg.Name), ""))
 
-			switch arg.Type {
-			case ArgumentTypeInt:
-				v, err := opt.IntValue()
-
-				if err != nil {
-					return nil, err
+				if argName != opt.Name {
+					continue
 				}
 
-				val = strconv.FormatInt(v, 10)
-			case ArgumentTypeUserMention:
-				v, err := opt.SnowflakeValue()
+				var val string
 
-				if err != nil {
-					return nil, err
+				switch arg.Type {
+				case ArgumentTypeInt:
+					v, err := opt.IntValue()
+
+					if err != nil {
+						return nil, err
+					}
+
+					val = strconv.FormatInt(v, 10)
+				case ArgumentTypeUserMention:
+					v, err := opt.SnowflakeValue()
+
+					if err != nil {
+						return nil, err
+					}
+
+					val = discord.UserID(v).Mention()
+				case ArgumentTypeChannelMention:
+					v, err := opt.SnowflakeValue()
+
+					if err != nil {
+						return nil, err
+					}
+
+					val = discord.ChannelID(v).Mention()
+				default:
+					val = opt.Value.String()
 				}
 
-				val = discord.UserID(v).Mention()
-			case ArgumentTypeChannelMention:
-				v, err := opt.SnowflakeValue()
+				args[arg.Index] = val
 
-				if err != nil {
-					return nil, err
-				}
-
-				val = discord.ChannelID(v).Mention()
-			default:
-				val = opt.Value.String()
+				break
 			}
-
-			args[arg.Index] = val
 		}
 	}
 
